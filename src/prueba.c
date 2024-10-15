5d7a4a87d9d72f1cd72d72b2aeb2c0566db785c0
Fernando Montero Erustes <fmontero@c1r9s5.42madrid.com>
2024-10-15 13:17:55 +0200

    pruebas en MLX

--- src/prueba.c
+++ src/prueba.c
@@ -1,23 +1,35 @@
-#include "MLX42/MLX42.h"
 #include <stdlib.h>
-#include <string.h>
-#define BPP sizeof(int32_t)
+#include <stdio.h>
+#include <stdio.h>
+#include "MLX42/MLX42.h"
+#define WIDTH 720
+#define HEIGHT 480
+
+void my_scrollhook(double xdelta, double ydelta, void* param)
+{
+	(void)param;
+	// Simple up or down detection.
+	if (ydelta > 0)
+		puts("Up!");
+	else if (ydelta < 0)
+		puts("Down!");
+	
+	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
+	if (xdelta < 0)
+		puts("Sliiiide to the left!");
+	else if (xdelta > 0)
+		puts("Sliiiide to the right!");
+}
 
 int32_t	main(void)
 {
-    mlx_t* mlx = mlx_init(256, 256, "MLX42", true);
-    if (!mlx)
-        exit(EXIT_FAILURE);
+	mlx_t* mlx;
 
-    mlx_image_t* img = mlx_new_image(mlx, 128, 128);
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+		return (EXIT_FAILURE);
 
-    // Set the channels of each pixel in our image to the maximum byte value of 255. 
-    memset(img->pixels, 255, img->width * img->height * BPP);
-
-    mlx_image_to_window(mlx, img, 0, 0);
-
-    // Run the main loop and terminate on quit.  
-    mlx_loop(mlx);
-    mlx_terminate(mlx);
-    return (EXIT_SUCCESS);
+	mlx_scroll_hook(mlx, &my_scrollhook, NULL);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
