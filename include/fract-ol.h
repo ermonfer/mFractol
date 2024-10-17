4a9c97155fea3906c0d432a7258761621b170ce4
Fernando Montero Erustes <fmontero@c1r9s1.42madrid.com>
2024-10-17 21:43:24 +0200

    jugando con imagenes y dando estructura

--- include/fract-ol.h
+++ include/fract-ol.h
@@ -13,8 +13,23 @@
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+# include "../lib/MLX42/include/MLX42/MLX42.h"
 # include <math.h>
 # include <stdbool.h>
+
+//Colors
+# define BLACK       		0x000000FF
+# define WHITE       		0xFFFFFFFF
+// Psychedelic colors
+# define NEON_ORANGE     	0xFF6600FF
+# define PSYCHEDELIC_PURPLE	0x660066FF
+# define AQUA_DREAM			0x33CCCCFF
+# define HOT_PINK        	0xE55982FF 
+# define CYAN_ELECTRIC		0xFF2CFFFF
+# define MCDONALDS			0xFFC72CFF
+# define SUPER_YELLOW		0xFCBE11FF
+# define PNKY_PASTEL		0xFFC4D6FF
+# define CUTE_GREEN			0xC1E378FF
 
 typedef unsigned int t_uint;
 
@@ -24,8 +39,19 @@ typedef struct s_complex
 	double	im;
 }	t_complex;
 
+typedef struct s_data
+{
+	mlx_t			*mlx;
+	mlx_image_t		*image;
+	t_complex		z;
+	t_complex		c;
+	int				color_theme;
+	int				color_index;
+}	t_data;
+
 t_complex	c_add(t_complex a, t_complex b);
 t_complex	c_mult(t_complex a, t_complex b);
 t_complex	c_square(t_complex a);
 double		c_modulus(t_complex a);
+t_uint		iter(t_complex z, t_complex c, t_uint check, t_uint limit);
 #endif
--- src/prueba.c
+++ src/prueba.c
@@ -1,34 +1,50 @@
+#include "../include/fract-ol.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdio.h>
-#include "MLX42/MLX42.h"
-#define WIDTH 720
-#define HEIGHT 480
+#include <unistd.h>
+#include <memory.h>
+#define WIDTH 1024
+#define HEIGHT 1024
 
-void my_scrollhook(double xdelta, double ydelta, void* param)
+static mlx_image_t	*g_img;
+
+void	hook(void* param)
 {
-	(void)param;
-	// Simple up or down detection.
-	if (ydelta > 0)
-		puts("Up!");
-	else if (ydelta < 0)
-		puts("Down!");
-	
-	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
-	if (xdelta < 0)
-		puts("Sliiiide to the left!");
-	else if (xdelta > 0)
-		puts("Sliiiide to the right!");
+	mlx_t*	mlx;
+	t_uint	x;
+	t_uint	y;
+
+	mlx = param;
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_P))
+		mlx_delete_image(mlx, g_img);
+	x = 0;
+	y = 0;
+	while (x < g_img->width)
+	{
+		while (y < g_img->height)
+		{
+			mlx_put_pixel(g_img, x, y, PNKY_PASTEL);
+			y++;
+		}
+		y = 0;
+		x++;
+	}
 }
 
 int32_t	main(void)
 {
-	mlx_t* mlx;
+	t_data		data;
+	data.mlx = mlx_init(WIDTH, HEIGHT, "fract-ol", false);
+	mlx_t*    mlx;
 
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-		return (EXIT_FAILURE);
-
-	mlx_scroll_hook(mlx, &my_scrollhook, NULL);
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", false);
+	if (!mlx)
+		exit(EXIT_FAILURE);
+	g_img = mlx_new_image(mlx, 1024, 1024);
+	mlx_image_to_window(mlx, g_img, 0, 0);
+	mlx_loop_hook(mlx, &hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
